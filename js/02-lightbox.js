861afc5ad02f8bb10f6ce97f88c59f09d381a9da
Анна Котляр <kotlyaranya1771@gmail.com>
2023-06-11 21:29:58 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const createItems = () => {
+  galleryEl.innerHTML = createImageEl(galleryItems).join('');
+};
+
+const createImageEl = (items) => {
+  return items.map(({ original, preview, description }) => `
+    <li class="gallery__item">
+        <a 
+            class="gallery__link"
+            href="${original}">
+                <img 
+                    class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}"
+                />
+        </a>
+    </li>`
+  );
+};
+
+createItems();
+
+const lightbox = new SimpleLightbox(`.gallery a`, { 
+    captionsData: "alt",  
+    captionType: "alt",
+    captionDelay: 250,
+});
